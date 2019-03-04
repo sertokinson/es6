e47b09df918af601d333a159faab11bcde1d6d09
Sergey Stakhanov <sertokinson@gmail.com>
2019-03-04 20:23:55 +0300

    Шаблонная строка

---
+++ src/main/app/js/templateString.js
@@ -0,0 +1,53 @@
+// Template string
+// ---------------
+
+// Syntax
+// `${dynamic_value}`;
+
+// Example 1 - string
+
+function greet(name) {
+  //console.log('Hello ' + name + '!');
+  console.log(`Hello ${name}!`);
+}
+greet('Max');
+// Example 2 - URL API string
+/*
+const URL = '/api';
+let path1 = 'test';
+let path2 = 'test2';
+let path3 = 'test3';
+//const API_URL = URL + '/' + path1 + '/' + path2 + '/' + path3;
+const API_URL = `${URL}/${path1}/${path2}/${path3}`;
+console.log(API_URL);
+*/
+// Example 3 - Multiple string
+
+function emailMessage(name, mail, status) {
+  return `
+    Hello ${name},
+    Your email ${mail} has been ${status}!
+    Thank you for subscription!
+  `
+}
+console.log(emailMessage('Jack', 'jack@mail.com', 'approved'));
+
+// Example 4 - Template
+/*
+generate() {
+    return `
+    <li class="news">
+      <a href="${this.url}" target="_blank" class="news__link">
+        <figure class="news__figure">
+          <img src="${this.urlToImage}" alt="${this.title}" class="news__image">
+          <figcaption class="news__description">
+            <h1 class="news__title">${this.title}</h1>
+            <p class="news__text">${this.description}</p>
+            <span class="news__date">${new Date(this.publishedAt).toLocaleDateString()}</span>
+            <cite class="news__author">${this.author}</cite>
+          </figcaption>
+        </figure>
+      </a>
+    </li>`;
+  }
+*/
