1bce032f645254bae61e0ede30eb0cc3b6e42c4b
Sergey Stakhanov <sertokinson@gmail.com>
2019-03-06 19:32:35 +0300

    for of

---
+++ src/main/app/js/for.js
@@ -0,0 +1,18 @@
+// for...of
+// ---------------
+
+const names = ['Jack', 'Max', 'Leo'];
+
+for(let name in names) {
+    //console.log(name);
+}
+
+
+for(let index in names) {
+    //console.log(names[index]);
+}
+
+
+for(let name of names) {
+    console.log(name);
+}
