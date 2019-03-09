af1affecdf6a96253c6d6d64e498411db637b0fc
Sergey Stakhanov <sertokinson@gmail.com>
2019-03-09 19:25:00 +0300

    наследование

---
+++ src/main/app/js/extends.js
@@ -0,0 +1,39 @@
+/ Inheritance
+// ---------------
+
+class Task {
+    constructor(title = '') {
+        this.title = title,
+            this.isCompleted = false
+    }
+
+    completed() {
+        this.isCompleted = true;
+    }
+}
+
+class SubTask extends Task {
+    constructor(title) {
+        super(title)
+    }
+
+    completed() {
+        super.completed();
+        console.log(`task ${this.title} is completed!`)
+    }
+}
+
+let task = new Task('Learn ReactJS');
+let subTask = new SubTask('Learn ES6');
+
+task.completed();
+subTask.completed();
+
+console.log(task);
+console.log(subTask);
+
+//class declaration
+class Task {};
+
+//class expression
+let Task = class Task {};
