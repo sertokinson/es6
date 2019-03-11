d20198acd1da69f9e0ec0add94ad0197b5d0e6e1
Sergey Stakhanov <sertokinson@gmail.com>
2019-03-11 20:56:49 +0300

    диструктивное присваивание для объектов

--- src/main/app/js/destructuring.js
+++ src/main/app/js/destructuring.js
@@ -24,3 +24,27 @@ function calculate([a,b]) {
 let [a, b] = calculate([1, 10]);
 
 console.log(a,b);
+
+// Object destructuring
+// ---------------
+
+let person = {
+    name: 'Jack',
+    age: 20,
+    address: {
+        city: 'Minsk'
+    }
+}
+
+//let {name = 'Max',age = 20} = {};
+//let { name: personName, age: personAge } = person;
+//let { address: { city = 'Brest' } } = person;
+//console.log(city);
+
+function getData({ name, age }) {
+    //console.log(name, age);
+    return { name, age }
+}
+
+let {name, age} = getData(person);
+console.log(name, age);
