e3ad62871b2074391b18283ba82b2a658dc85056
Sergey Stakhanov <sertokinson@gmail.com>
2019-03-13 20:41:40 +0300

    промисы

---
+++ src/main/app/js/promise.js
@@ -0,0 +1,25 @@
+// Promise
+// ---------------
+
+// const promise = new Promise((resolve, reject) => {
+//   throw new Error('some error...');
+//   setTimeout(() => {
+//     if(true) {
+//       resolve('promise completed!');
+//     } else {
+//       reject();
+//     }
+//   }, 1000);
+// });
+//
+//
+// promise
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
+
+fetch('https://jsonplaceholder.typicode.com/posts/')
+    .then(res => res.json())
+    .then(data => data)
+    .then(data => console.log('!!!', data))
+    //.then(data => render(data))
+    .catch(() => console.log('some error...'));
