a0ac7e8d4d4fc5a23c95273c45d2f5e25a0fd1e9
Sergey Stakhanov <sertokinson@gmail.com>
2019-03-14 19:58:01 +0300

    async/await

---
+++ src/main/app/js/asyncAwait.js
@@ -0,0 +1,46 @@
+// Async/Await
+// ---------------
+/*
+const fetchData = () => Promise.resolve({
+  data: ['Jack', 'Max', 'Leo']
+})
+const getNamesData = () => {
+  fetchData()
+    .then(data => {
+      console.log(data);
+      return 'done';
+    })
+}
+const getNamesData2 = async () => {
+  console.log(await fetchData());
+  return 'done';
+}
+getNamesData();
+getNamesData2();
+console.log(getNamesData2());
+const fetchData = () => Promise.reject('some error...');
+const getNamesData = async () => {
+  try {
+    console.log(await fetchData());
+  } catch(error) {
+    console.log(error);
+  }
+}
+getNamesData();
+*/
+
+const load = () => {
+    Promise.resolve(5)
+        .then(a => {
+            Promise.resolve(10)
+                .then(b => console.log(a+b))
+        })
+}
+load();
+
+const load2 = async () =>{
+    const a = await Promise.resolve(5);
+    const b = await Promise.resolve(10);
+    console.log(a+b)
+}
+load2();
