a4b55d4f818b512d63f820830320db2aa7b275f6
Sergey Stakhanov <sertokinson@gmail.com>
2019-03-17 14:58:10 +0300

    export and import

---
+++ src/main/app/js/export_import.js
@@ -0,0 +1,43 @@
+// Modules
+// ---------------
+// Variables
+export let one = 1;
+
+let two = 2;
+let three = 3;
+export { two, three };
+
+// Class
+class Person {
+    constructor(name) {
+        this.name = name
+    }
+}
+
+// Function
+function sayHello() {
+    console.log('hello!')
+}
+
+export { Person, sayHello }
+
+// Rename
+export { one as once, two as twice };
+
+//Import
+import { one, two } from './file.js';
+
+// Rename Import
+import { one as num1, two as num2 } from './file.js';
+
+// One Import
+import * as numbers from './file.js';
+
+// default export
+export default class Person {
+    constructor(name) {
+        this.name = name
+    }
+}
+
+import Person from './file.js';
